a997feba93274db0a73ccdd8dfce680af9a99f3a
Alkazuz <kerallis2015@gmail.com>
2022-01-17 00:11:36 -0300

    Initial commit

---
+++ ChestStealer/CMDChestSteal.cs
@@ -0,0 +1,65 @@
+﻿using AdvancedBot.client;
+using AdvancedBot.client.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChestStealer
+{
+    class CMDChestSteal : CommandBase
+    {
+        public MinecraftClient client;
+
+        public MinecraftClient getClient()
+        {
+            return this.client;
+        }
+        public CMDChestSteal(MinecraftClient cli) : base(cli,
+            "ChestStealer", //Titulo
+            "Takes all items from an open chest for inventory", //Descrição
+            new string[] {
+                "cheststeal", "cheststealer" //Uso do comando, se usar algum desses será executado
+            })
+        {
+            this.client = cli;
+        }
+
+        public override CommandResult Run(string alias, string[] args)
+        {
+            steal();
+            return CommandResult.Success;
+        }
+
+        public async void steal()
+        {
+            Inventory invOpen = Client.OpenWindow;
+            Inventory inv = Client.Inventory;   
+            if (invOpen != null)
+            {
+                int slot = 9;
+                for (int i = 0; i < invOpen.NumSlots; i++)
+                {
+                    ItemStack item = invOpen.Slots[i];
+                    if (item != null)
+                    {
+                        invOpen.Click(Client, (short)i, false, true);
+                        await Task.Delay(500);
+                        ItemStack currentItemSlot = inv.Slots[slot];
+                        while(currentItemSlot != null)
+                        {
+                            slot++;
+                            if(slot > 35) { return; }
+                            currentItemSlot = inv.Slots[slot];
+                            
+                        }
+                        inv.Click(Client, (short)slot, true, true);
+                        await Task.Delay(500);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ ChestStealer/Main.cs
@@ -0,0 +1,50 @@
+﻿using AdvancedBot;
+using AdvancedBot.client;
+using AdvancedBot.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChestStealer
+{
+    public class Main : IPlugin
+    {
+        private CMDChestSteal cMDChestSteal;
+
+        public Main()
+        {
+            foreach(MinecraftClient client in Program.FrmMain.Clients)
+            {
+                if (client.CmdManager.GetCommand<CMDChestSteal>() == null)
+                    cMDChestSteal = new CMDChestSteal(client);
+                    client.CmdManager.Commands.Add(cMDChestSteal);
+            }
+        }
+        public void onClientConnect(MinecraftClient client)
+        {
+            if (client.CmdManager.GetCommand<CMDChestSteal>() == null)
+                cMDChestSteal = new CMDChestSteal(client);
+                client.CmdManager.Commands.Add(cMDChestSteal);
+        }
+
+        public void onReceiveChat(string chat, byte pos, MinecraftClient client)
+        {}
+
+        public void OnReceivePacket(ReadBuffer pkt, MinecraftClient client)
+        {}
+
+        public void onSendChat(string chat, MinecraftClient client)
+        {}
+
+        public void OnSendPacket(IPacket packet, MinecraftClient client)
+        {}
+
+        public void Tick()
+        {}
+
+        public void Unload()
+        {}
+    }
+}
